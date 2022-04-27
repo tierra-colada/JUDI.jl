8262c980c511ec8f6cd39e9b0d507203e479a0e5
mloubout <mathias.louboutin@gmail.com>
2022-04-26 20:07:43 -0400

    non-array correct

--- src/rrules.jl
+++ src/rrules.jl
@@ -1,13 +1,23 @@
 ############################ AD rules ############################################
 function rrule(F::judiPropagator{T, O}, m::AbstractArray{T}, q::AbstractArray{T}) where {T, O}
     y = F(m, q)
+    dims = length(q)
     function pullback(Δy)
-        dq = @thunk(reshape(F'(m, Δy), length(q)))
+        dq = @thunk(diffsource(F, m, Δy, dims))
         dm = @thunk(reshape(judiJacobian(F, q)'(m, Δy), size(m)))
         return (NoTangent(), dm, dq)
     end
     return y, pullback
 end
+
+function diffsource(F::judiPropagator{T, O}, m::AbstractArray{T}, Δy, dims::Integer) where {T, O}
+    ra = F.options.return_array
+    dq = F'(m, Δy)
+    # Reshape if vector
+    dq = ra ? reshape(dq, dims) : dq
+    return dq
+end
+
 
 # projection
 (project::ProjectTo{AbstractArray})(dx::PhysicalParameter) = project(reshape(dx.data, project.axes))
